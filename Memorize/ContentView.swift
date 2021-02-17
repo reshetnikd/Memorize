fa897fb08ec9077e12975c1a4ae85848be168b33
reshetnikd <dmitry.reshetnik@icloud.com>
2021-02-17 22:50:25 +0200

    Force each card to have a width to height ratio of 2:3

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                     .onTapGesture {
                         viewModel.choose(card: card)
                     }
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .padding()
